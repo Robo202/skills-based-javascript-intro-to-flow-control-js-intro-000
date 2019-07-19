4bb0cfbb07ddc6aa3ec5aea42e4ffebcfb8487cf
active-researcher-7876 <elliott.daigle@gmail.com>
2019-07-19 01:20:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,10 +1,10 @@
 function basicTeenager(age) {
-  if ((age >=13) && (age <=19))
+  if ((age >= 13) && (age <= 19))
   return "You are a teenager!"
   }
 
 function teenager(age) {
-  if ((age >=13) && (age <=19))
+  if ((age >= 13) && (age <= 19))
   return "You are a teenager!"
 }
   else{
@@ -12,13 +12,13 @@ function teenager(age) {
   }
 
 function ageChecker(age) {
-  if ((age >=13) && (age <=19))
+  if ((age >= 13) && (age <= 19))
   return "You are a teenager!"
   }
-  else if (age <=12){
+  else if (age <= 12){
   return "You are a kid"
 }
-  else (age >=19){
+  else (age >= 19){
     return "You are a grownup"
   }
   else{
