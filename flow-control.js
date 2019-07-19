a106e11e0c000a2750caa585aba357b261c67656
active-researcher-7876 <elliott.daigle@gmail.com>
2019-07-19 01:31:56 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,15 +1,15 @@
 function basicTeenager(age) {
   if ((age >= 13) && (age <= 19))
   return "You are a teenager!"
-  }
+}
 
 function teenager(age) {
   if ((age >= 13) && (age <= 19))
   return "You are a teenager!"
-}
+  }
   else{
     return "You are not a teenager"
-  }
+}
 
 function ageChecker(age) {
   if ((age >= 13) && (age <= 19))
@@ -17,7 +17,7 @@ function ageChecker(age) {
   }
   else if (age <= 12){
   return "You are a kid"
-}
+  }
   else (age >= 19){
     return "You are a grownup"
   }
@@ -27,9 +27,14 @@ function ageChecker(age) {
 
 function ternaryTeenager(age) {
   return ((age >= 13) && (age <= 19)) ? "You are a teenager" : "You are not a teenager"
-
 }
 
 function switchAge(age) {
-
+  switch(age) {
+    case "13", "14", "15", "16", "17", "18", "19":
+    console.log("You are a teenager");
+    break;
+    default:
+    console.log("You have an age");
+  }
 }
